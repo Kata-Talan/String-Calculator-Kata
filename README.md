62c56e5e77d8218d631be94c90741ca9e4e3f651
Vivien AUGUY <vivien.auguy@talan.com>
2021-04-14 13:34:52 +0200

    test for simple case

--- README.md
+++ README.md
@@ -4,10 +4,10 @@ It’s an incremental kata to simulate a real business situation: start of by re
 ## Step 1
 Create a simple String calculator with a method signature:
 
-    int Add(string numbers)
+    int add(string numbers)
 The method can take up to two numbers, separated by commas, and will return their sum.
 
-For example ` or `1` or `1,2` as inputs.
+For example `` or `1` or `1,2` as inputs.
 
 For an empty string it will return 0.
 
--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -1,6 +1,18 @@
 import Calculator from "./calculator";
 
-describe('tennis', () => {
+describe('calculator', () => {
 
+	let calculator = new Calculator();
 
+	it('should return 0 when the string is empty', () => {
+		expect(calculator.add('')).toEqual(0)
+	})
+
+	it('should return the number when the string contains a number', () => {
+		expect(calculator.add('3')).toEqual(3)
+	})
+
+	it('should return the sum of two numbers separated with a comma', () => {
+		expect(calculator.add('3,2')).toEqual(5)
+	})
 });
