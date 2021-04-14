dd82fc6c2c40ea8ff5d79e8bb0f57a390e1c6158
jonathan.baranzini <jonathan.baranzini@talan.com>
2021-04-14 13:37:44 +0200

    Implement simple add

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,5 +1,14 @@
 class Calculator {
-
+	add(value: string):number {
+		if(value === ""){
+			return 0;
+		}
+		if(value.indexOf(',') === -1){
+			return parseInt(value);
+		}
+		let values = value.split(',')
+		return parseInt(values[0]) + parseInt(values[1]);
+	}
 
 }
 
