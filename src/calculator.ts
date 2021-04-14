c0fcababad8473786f30e3bbd5207af034aec4de
Vivien AUGUY <vivien.auguy@talan.com>
2021-04-14 14:00:55 +0200

    implem step 4

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,8 +1,14 @@
 class Calculator {
 	add(value: string): number {
-		return value.split(/,|\n/)
-		            .reduce((accumulator, currentValue) =>
-			            accumulator + parseInt(currentValue) || 0, 0)
+		let data = value;
+		let separators: RegExp | string = /,|\n/;
+		if (value.startsWith("//")) {
+			separators = value[2]
+			data = value.substring(4)
+		}
+		return data.split(separators)
+		           .reduce((accumulator, currentValue) =>
+			           accumulator + parseInt(currentValue) || 0, 0)
 	}
 }
 
