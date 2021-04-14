cdcbb543af0b222a51862241060a508605e2081d
Vivien AUGUY <vivien.auguy@talan.com>
2021-04-14 13:45:15 +0200

    Implement step 2

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,15 +1,9 @@
 class Calculator {
-	add(value: string):number {
-		if(value === ""){
-			return 0;
-		}
-		if(value.indexOf(',') === -1){
-			return parseInt(value);
-		}
-		let values = value.split(',')
-		return parseInt(values[0]) + parseInt(values[1]);
+	add(value: string): number {
+		return value.split(',')
+		            .reduce((accumulator, currentValue) =>
+			            accumulator + parseInt(currentValue) || 0, 0)
 	}
-
 }
 
 export default Calculator;
