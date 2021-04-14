12911f06786f78ae7380a2a2c31fc6cc4e825f06
Vivien AUGUY <vivien.auguy@talan.com>
2021-04-14 13:47:18 +0200

    Add tests for step 3

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -19,4 +19,12 @@ describe('calculator', () => {
 	it('should return the sum of more than 2.5 numbers separated with a comma', () => {
 		expect(calculator.add('3,2,5,12,54,23')).toEqual(99)
 	})
+
+	it('should return the sum of two numbers separated with a new line', () => {
+		expect(calculator.add('3\n2')).toEqual(5)
+	})
+
+	it('should return the sum of multiple numbers separated with multiple separators', () => {
+		expect(calculator.add('3,2,5\n12,54\n23')).toEqual(5)
+	})
 });
